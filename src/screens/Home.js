fd5e055cdaa9ba85daa7751bfdc29161558bf33b
조현일 <himzei@gmail.com>
2022-11-27 12:30:06 +0900

    insta front

---
+++ src/screens/Home.js
@@ -0,0 +1,11 @@
+import { isLoggedInVar, logUserOut } from "../apollo";
+
+function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <button onClick={() => logUserOut()}>Log out now!</button>
+    </div>
+  );
+}
+export default Home;
